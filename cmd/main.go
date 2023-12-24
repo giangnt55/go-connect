aca56103716b5eb19c1449b3034293e96049e6b8
Truong Giang <giangntse150747@gmail.com>
2023-12-24 22:45:09 +0700

    add gin dependencies

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Print("Init source")
+	fmt.Println("Initializing REST API...")
+
+	// Create a default Gin router
+	router := gin.Default()
+
+	// Define a simple route
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to the REST API with Gin!",
+		})
+	})
+
+	// Run the server on port 8080
+	err := router.Run(":8080")
+	if err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
 }
